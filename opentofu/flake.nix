c7abe711ebcda06095c338226d559a470fbf80a4
Arnaud Béguin <abeguin@pictet.com>
2025-07-18 07:18:01 +0200

    feat: opentofu flake

---
+++ opentofu/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Nix flake for terraform projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "terraform dev shell";
+          packages = with pkgs; [
+            opentofu
+            #awscli2
+
+            xc
+            cocogitto
+          ];
+        };
+      }
+    );
+}
