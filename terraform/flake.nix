f8797081c8d17587dfbb786c84ede973fd441552
Arnaud Béguin <abeguin@pictet.com>
2025-07-18 07:10:11 +0200

    feat: terraform flake

---
+++ terraform/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Nix flake for terraform projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          config.allowUnfree = true;
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "terraform dev shell";
+          packages = with pkgs; [
+            terraform
+            #awscli2
+
+            xc
+            cocogitto
+          ];
+        };
+      }
+    );
+}
